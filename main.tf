006a90c14498d63cc34400173e91169ef1995d0a
rodrigo andrade <rodrigo.andrade@retta.com.br>
2022-12-26 15:08:40 -0300

    Modificando host e iniciando pelo ansible

--- main.tf
+++ main.tf
@@ -24,6 +24,6 @@ resource "aws_instance" "app_server" {
   #                nohup busybox httpd -f -p 8080 &
   #                EOF
   tags = {
-    Name = "InstanceUseTerraform2"
+    Name = "TerraformAnsiblePython"
   }
 }
